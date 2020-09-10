ce85f924a16a877073e8034d9cc594713db79334
Harry You <fntldpf12@gmail.com>
2020-09-10 18:04:24 +0900

    start: first commit of the project

---
+++ ToDoList/script/AddTask.js
@@ -0,0 +1,12 @@
+﻿let addIcon = document.querySelector("#add");
+let taskInput = document.querySelector("#taskInput");
+
+addIcon.addEventListener("click", function () {
+    addIcon.classList.add("addTaskDisappear");
+    
+    setTimeout(function () {
+        addIcon.style.display = "none";
+        addIcon.classList.remove("addTaskDisappear")
+        taskInput.classList.add("taskInputAppear");
+    }, 500);
+})
