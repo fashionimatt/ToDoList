cb19d4f95a6680f79ce3f38e9ee397137096fec9
Harry You <fntldpf12@gmail.com>
2020-09-14 17:22:37 +0900

    add: 작업 추가, 완료, 삭제 기능 구현 완료
    추가 css 작업 필요
    완료된 작업 목록 및 진행 중인 작업 목록 구분 구현 필요

--- ToDoList/script/AddTask.js
+++ ToDoList/script/AddTask.js
@@ -1,16 +1,56 @@
-﻿let add = document.querySelector("#add");
-let addText = document.querySelector("#addText");
-let taskInput = document.querySelector("#taskInput");
+﻿let enterButton = document.querySelector("#enter");
+let input = document.querySelector("#userInput");
+let ul = document.querySelector("ul");
+let item = document.querySelector("li");
+let n = 0;
 
-add.addEventListener("click", function () {
-    addText.classList.add("addTaskDisappear");
-    taskInput.style.display = "inline-block";
-    
-    setTimeout(function () {
-        addText.style.display = "none";
-        addText.classList.remove("addTaskDisappear")
-        taskInput.classList.add("taskInputAppear");
-    }, 500);
+function inputLength() {
+    return input.value.length;
+}
 
-    
-})
+function listLength() {
+    return item.length;
+}
+
+function createListElement() {
+    let li = document.createElement("li");
+    li.id = "item" + n;
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+
+
+    function crossOut() {
+        li.classList.toggle("done");
+    }
+
+    li.addEventListener("click", crossOut);
+
+    let delBtn = document.createElement("button");
+    delBtn.appendChild(document.createTextNode("X"));
+    li.appendChild(delBtn);
+    delBtn.addEventListener("click", deleteListItem);
+
+    n += 1;
+
+    function deleteListItem(e) {
+        let delTarget = document.querySelector("#" + delBtn.parentElement.id);
+        let parentDiv = delTarget.parentElement;
+        parentDiv.removeChild(delTarget);
+    }
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
+    }
+}
+
+function addListAfterKeypress(e) {
+    if (inputLength() > 0 && e.which === 13) {
+        createListElement();
+    }
+}
+
+enterButton.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
